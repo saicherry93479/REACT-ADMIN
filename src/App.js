f6c7d13c1a7c1b7f42840045928693d4214bc92f
saicherry93479 <saicherry93479@gmail.com>
2022-03-08 09:27:51 +0530

    ghpages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Main from "./Components/Admin/Main";
 import NewPost from "./Components/Admin/NewPost";
@@ -12,7 +12,7 @@ import Login from "./Components/Login/Login";
 const App = () => {
   return (
     <Router>
-      <Routes >
+      <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/admin" element={<Main />}>
